43c259a3c2bd4088e63d75b96eb1c4ce21b2bbbf
camara92 <88945535+camara92@users.noreply.github.com>
2022-04-05 10:29:22 +0200

    PROJECTS FILES

---
+++ mon-programme.js
@@ -0,0 +1,30 @@
+/*
+ * Fichier : mon-programme.js
+ */
+
+
+//console.log('pppp');
+
+$(document).ready(function(){
+
+
+	$('.iconMenuBurger').on("click", function(){
+		$('.menuWrapper nav').slideToggle(250);
+	});
+
+	$('.slideshow').slick({
+		arrows: false,
+		fade: true,
+		pauseOnHover: false,
+		autoplay: true,
+		autoplaySpeed: 15000
+	});
+
+	$('.sliderVoiture').slick({
+		prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-caret-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="fas fa-caret-right"></i></button>'
+	});	
+
+
+
+});
